756ddf8bb2975ad25b2ba532c83058330d863ea7
Alexey Kabaiev <kabaievalex@gmail.com>
2023-04-14 15:48:08 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
+import React from "react";
+import Counter from './components/Counter/Counter';
+
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Состояние компонента</h1>
+      <Counter initialValue={10} />
+    </>
   );
 };
+
+export default App;
